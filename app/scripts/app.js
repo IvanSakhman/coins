700428cc2f2f4f67838685170b93970304cab7a4
Ivan Sakhman <Sakhman.I@gmail.com>
2017-02-23 19:04:55 +0200

    add project

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,5 +2,23 @@ import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
 
 $(() => {
-	svg4everybody();
+    let cross = $( ".cross" );
+    let menu = $( ".menu" );
+    let hamburger = $( ".hamburger" );
+
+    cross.hide();
+    menu.hide();
+    hamburger.click(function() {
+        menu.slideToggle( 100, function() {
+            hamburger.hide();
+            cross.show();
+        });
+    });
+
+    cross.click(function() {
+        menu.slideToggle( 100, function() {
+            cross.hide();
+            hamburger.show();
+        });
+    });
 });
